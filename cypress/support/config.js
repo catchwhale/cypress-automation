5651b1c856dd3abb8094d2dbee09e0b5b943f27c
catchwhale <mrjoe8888@gmail.com>
2022-05-07 23:41:43 +0800

    adding dependencies

---
+++ cypress/support/config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Joseph Ocero
+ * @Date: 2022-05-07 22:48:43
+ * @LastEditors: Joseph ocero
+ * @LastEditTime: 
+ * @Description: file content
+ */
+
+
+
+//json
+import Message from "./json/message.json";
+
+export {
+	Message, //all json
+}
